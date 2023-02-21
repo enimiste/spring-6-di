cde8d8aa832600b6779b47c371f30c1ba8820d91
enimiste <nouni.elbachir@gmail.com>
2023-02-21 01:14:56 +0100

    add service

--- src/main/java/guru/springframework/springdi/controllers/MyController.java
+++ src/main/java/guru/springframework/springdi/controllers/MyController.java
@@ -1,11 +1,18 @@
 package guru.springframework.springdi.controllers;
 
+import guru.springframework.springdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+    private final GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
 
     public String sayHello() {
-        return "Hello from @%s controller object".formatted(Integer.toHexString(hashCode()));
+        return "%s from @%s controller object".formatted(greetingService.greeting(),
+                Integer.toHexString(hashCode()));
     }
 }
---
+++ src/main/java/guru/springframework/springdi/services/GreetingServiceImpl.java
@@ -0,0 +1,11 @@
+package guru.springframework.springdi.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingServiceImpl implements GreetingService {
+    @Override
+    public String greeting() {
+        return "Hello from Base service !!!";
+    }
+}
